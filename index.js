0399cc270b3565be44c031d657ae517c3bfe1501
maildowirusa1-lgtm <maildowirusa1@gmail.com>
2026-02-24 01:38:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Serwer działa");
+// Lista klientów i dostęp
+let clients = {
+  "klinikaDentystycznaKrawczyk": { access_granted: true }
+};
+
+// Endpoint sprawdzający dostęp klienta
+app.get("/check_access", (req, res) => {
+  const client_id = req.query.client_id;
+  if(clients[client_id] && clients[client_id].access_granted) {
+    res.json({ access_granted: true });
+  } else {
+    res.json({ access_granted: false });
+  }
+});
+
+// Endpoint pliku JS dla klienta
+app.get("/asystent.js", (req, res) => {
+  res.type(".js");
+  res.send(`
+(function(){
+  const client_id = "klinikaDentystycznaKrawczyk";
+  
+  fetch("https://express-js-on-verce-epwiw6pyb.vercel.app/check_access?client_id=" + client_id)
+    .then(res => res.json())
+    .then(data => {
+      if(data.access_granted){
+        let script = document.createElement("script");
+        script.src = "TU_WKLEJ_LINK_DO_NOUPE"; // <- wklej tutaj swój link do widgetu Noupe
+        document.head.appendChild(script);
+      }
+    });
+})();
+  `);
 });
 
 module.exports = app;
